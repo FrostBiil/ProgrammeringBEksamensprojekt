a2eb0b5a8f31aed5b4860c933ccfc324ab752b21
T3CH <jlp02.dk@gmail.com>
2024-03-11 13:56:08 +0100

    Fixed the template to work and replaces sample code

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,35 @@
-// Server code goes here
+import Express, { Application } from 'express'
+import Cors from 'cors'
+
+/**
+ * Singelton class to create and manage an express server
+ */
+export default class Server {
+    private application: Application
+    private port: number | string
+
+    constructor() {
+        this.port = process.env.PORT || 3000
+        this.application = Express()
+    }
+
+    private plugins() {
+        this.application.use(Express.urlencoded({ extended: true }))
+        this.application.use(Express.json())
+        this.application.use(Cors())
+        //   this.application.use(Routes)
+    }
+
+    public run() {
+        try {
+            this.plugins()
+            this.application.listen(this.port, () => {
+                console.log(`Server running on http://localhost:${this.port}`)
+            })
+        } catch (err) {
+            console.error(err)
+            process.exit(1)
+        }
+    }
+}
+new Server().run()
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
-// Client code goes here
+import react from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<h1>React App!</h1>, root)
