8a622ba59d64ea4012845a3a1d14e6364fa29ad4
T3CH <jlp02.dk@gmail.com>
2024-03-12 22:38:08 +0100

    Added comments

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,12 +3,14 @@ import { Api } from '../utils/api'; // Juster stien efter din filstruktur
 
 function LoginPage() {
   const handleLogin = async () => {
+    // Api.login() vil redirecte til den side hvor brugeren var da funktionen blev kaldt
     Api.login();
   };
 
   useEffect(() => {
     Api.me().then((user) => {
-      console.log(user);
+      // Hvis brugeren er logget ind, så vil user være en object med brugerens information
+      console.log(user.data);
     });
   }, []);
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -16,23 +16,25 @@ export class Api {
         return window.fetch(this.getEndpoint(endpoint), {
             method: method,
             body: typeof body === "string" ? body : JSON.stringify(body),
+
+            // Nødvendigt for at kunne sende cookies med i fetch requesten, men kun i development da de kører på samme server i production
             credentials: IS_PRODUCTION ? "same-origin" : "include",
         })
     }
 
     public static login(redirectSuccess?: string) {
-        // URL encode hvor den skal redirect til efter login
+        // URL encode hvor den skal redirect til efter login (hvis ikke angivet, så redirecter den til den nuværende side)
         const currentUrl = window.location.href;
         const redirect = encodeURIComponent(redirectSuccess || currentUrl);
         
         console.log(redirect);
 
-        // Redirect til /api/auth/login
+        // Redirect til /api/auth/github?redirect=URL
         window.location.href = this.getEndpoint(`/auth/github?redirect=${redirect}`);
     }
 
     public static async me() {
-        /* Fetch /api/auth/me */
+        // Fetch /api/auth/me
         const res = await this.fetch("/auth/me");
         if (res.status === 200) {
             return res.json();
