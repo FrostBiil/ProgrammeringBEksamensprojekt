a32d2a3efd4e712b32b557ae87603569ca461b06
Matthias <Matthiasfrostbiil@gmail.com>
2024-04-17 12:18:37 +0200

    Update LibraryPage with loading

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -8,40 +8,34 @@ import {
   Group,
   Flex,
   Pill,
+  Center,
+  Loader,
 } from "@mantine/core";
 import { Game, GameOwner } from "@prisma/client";
 import React, { useContext, useEffect, useState } from "react";
 import { Api } from "../utils/api";
 import { AuthContext } from "../contexts/AuthProvider";
 
-
 export function LibraryPage() {
   const [ownerships, setOwnerships] = useState<GameOwner[]>([]);
   const { user, loaded } = useContext(AuthContext);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     Api.getUserGames().then((ownerships) => {
       console.log(ownerships);
       setOwnerships(ownerships.map((ownership) => ownership));
+      setLoading(false);
     });
   }, []);
 
   useEffect(() => {
-    console.log(user)
+    console.log(user);
     if (!user && loaded) {
       Api.login();
     }
   }, [user, loaded]);
-
-  const downloadGameButton = (item: Game) => {
-    return (
-      <Group mt="xs">
-        <Button size="sm" onClick={() => window.open(item.projectUrl + "/releases/latest")}>
-          Download spil
-        </Button>
-      </Group>
-    );
-  };
 
   const goToGamePageButton = (item: Game) => {
     return (
@@ -51,53 +45,76 @@ export function LibraryPage() {
         </Button>
       </Group>
     );
-  }
+  };
 
-  const games = (span?: number, sortedByDate?: boolean) => ownerships
-    .sort((a, b) => sortedByDate ? (new Date(b.date).getTime() - new Date(a.date).getTime()) : (a as any as { game: Game }).game.title.localeCompare((b as any as { game: Game }).game.title))
-    .map(o => (o as any as { game: Game }).game).map((item) => (
-      <Grid.Col key={item.id} span={span ?? 2} p="lg">
-        <Image
-          style={{ aspectRatio: 16 / 9 }}
-          radius="md"
-          src={item.cover}
-          alt={item.title}
-        />
-        <Title order={3} pt="md">
-          {item.title}
-        </Title>
-        <Text c="gray" pb="md">
-          {(item.genres ?? []).join(", ")}
-        </Text>
-        <Text fz="sm" mt="xs">
-          {item.description}
-        </Text>
-        {item.tags && item.tags.length > 0 ? (
-          <>
-            <Divider mt="md" labelPosition="left" label="Tags" />
-            <Flex wrap={"wrap"} gap="md" pt={"md"}>
-              {item.tags.map((tag) => (
-                <Pill key={tag} color="blue" style={{ marginRight: 5 }}>
-                  {tag}
-                </Pill>
-              ))}
-            </Flex>
-            <Divider mt="xs" />
-          </>
-        ) : (
-          <></>
-        )}
-        {goToGamePageButton(item)}
-      </Grid.Col>
-    ));
+  const games = (span?: number, sortedByDate?: boolean) =>
+    ownerships
+      .sort((a, b) =>
+        sortedByDate
+          ? new Date(b.date).getTime() - new Date(a.date).getTime()
+          : (a as any as { game: Game }).game.title.localeCompare(
+              (b as any as { game: Game }).game.title
+            )
+      )
+      .map((o) => (o as any as { game: Game }).game)
+      .map((item) => (
+        <Grid.Col key={item.id} span={span ?? 2} p="lg">
+          <Image
+            style={{ aspectRatio: 16 / 9 }}
+            radius="md"
+            src={item.cover}
+            alt={item.title}
+          />
+          <Title order={3} pt="md">
+            {item.title}
+          </Title>
+          <Text c="gray" pb="md">
+            {(item.genres ?? []).join(", ")}
+          </Text>
+          <Text fz="sm" mt="xs">
+            {item.description}
+          </Text>
+          {item.tags && item.tags.length > 0 ? (
+            <>
+              <Divider mt="md" labelPosition="left" label="Tags" />
+              <Flex wrap={"wrap"} gap="md" pt={"md"}>
+                {item.tags.map((tag) => (
+                  <Pill key={tag} color="blue" style={{ marginRight: 5 }}>
+                    {tag}
+                  </Pill>
+                ))}
+              </Flex>
+              <Divider mt="xs" />
+            </>
+          ) : (
+            <></>
+          )}
+          {goToGamePageButton(item)}
+        </Grid.Col>
+      ));
+
+  if (loading)
+    return (
+      <Center pt={"xl"}>
+        <Loader />
+      </Center>
+    );
 
   return (
     <>
       <Divider mx="md" labelPosition="left" label={"Senest spil"} />
-      <Grid columns={3} p="md">{games(1, true).slice(0, 3)}</Grid>
+      <Grid columns={3} p="md">
+        {games(1, true).slice(0, 3)}
+      </Grid>
 
-      <Divider mx="md" labelPosition="left" label={"Spil " + ownerships.length} />
-      <Grid columns={5} p="md">{games(1)}</Grid>
+      <Divider
+        mx="md"
+        labelPosition="left"
+        label={"Spil " + ownerships.length}
+      />
+      <Grid columns={5} p="md">
+        {games(1)}
+      </Grid>
     </>
   );
 }
