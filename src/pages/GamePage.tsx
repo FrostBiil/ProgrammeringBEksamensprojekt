2002d7aca580e4139db74f964c5221d0f0cf39f9
Matthias <Matthiasfrostbiil@gmail.com>
2024-04-17 12:57:54 +0200

    Update GamePage.tsx
    Made error if player tries to reach a nonexistant game id.

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -15,8 +15,11 @@ import {
   Group,
   Center,
   Loader,
+  SimpleGrid,
+  Title,
 } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
+import { ErrorPage } from "./ErrorPage";
 
 interface Game {
   id: string;
@@ -57,6 +60,10 @@ export function GamePage() {
     );
   };
 
+  function goToHome() {
+    window.location.href = "/";
+  }
+
   useEffect(() => {
     if (id === undefined) {
       return;
@@ -68,12 +75,34 @@ export function GamePage() {
     });
   }, [id]);
 
-  if (loading || game === null)
+  if (loading)
     return (
       <Center pt={"xl"}>
         <Loader />
       </Center>
     );
+
+  if (game === null) {
+    return (
+      <Container pt="80px" pb="80px">
+        <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
+          <div>
+            <Title fw="900" fs="34px" mb="md" ff="arial">
+              Der er noget galt...
+            </Title>
+            <Text c="dimmed" size="lg">
+              Siden du forsøger at tilgå eksistere ikke. Du kan have skrevet den
+              forkerte webadresse, eller siden er blevet flyttet til en anden
+              URL. Hvis dette er en fejl, kontakt support.
+            </Text>
+            <Button onClick={goToHome} variant="outline" size="md" mt="xl">
+              Gå tilbage til forsiden
+            </Button>
+          </div>
+        </SimpleGrid>
+      </Container>
+    );
+  }
 
   return (
     <>
