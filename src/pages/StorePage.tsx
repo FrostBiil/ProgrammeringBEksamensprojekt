46fd0b22f1263d829f949f42574c8adaa43794dd
Matthias <Matthiasfrostbiil@gmail.com>
2024-04-17 12:43:57 +0200

    Update StorePage.tsx

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -26,7 +26,6 @@ import { GameCardLarge } from "../components/GameCardLarge";
 import { useDebouncedState } from "@mantine/hooks";
 
 export function StorePage() {
-
   const [games, setGames] = useState<Game[]>([]);
   const theme = useMantineTheme();
   const { user } = useContext(AuthContext);
@@ -37,9 +36,12 @@ export function StorePage() {
 
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
-    Api.getGames(search.length > 0 ? search : undefined, genres && (genres?.length > 0) ? genres : undefined, tags.length > 0 ? tags : undefined).then((games) => {
+    Api.getGames(
+      search.length > 0 ? search : undefined,
+      genres && genres?.length > 0 ? genres : undefined,
+      tags.length > 0 ? tags : undefined
+    ).then((games) => {
       console.log(games);
       setGames(games);
       setLoading(false);
@@ -49,16 +51,23 @@ export function StorePage() {
   const addGameButton = (item: Game) => {
     return user ? (
       <SimpleGrid mt={"md"} cols={2} w={"100%"}>
-        <Button size="sm" variant="outline" onClick={() => {
-          window.location.href = `/spil/${item.id}`;
-        }} >
+        <Button
+          size="sm"
+          variant="outline"
+          onClick={() => {
+            window.location.href = `/spil/${item.id}`;
+          }}
+        >
           Se detaljer
         </Button>
-        <Button size="sm" onClick={() => {
-          Api.addGameToUser(item.id);
+        <Button
+          size="sm"
+          onClick={() => {
+            Api.addGameToUser(item.id);
 
-          setGames(games.filter((game) => game.id !== item.id));
-        }}>
+            setGames(games.filter((game) => game.id !== item.id));
+          }}
+        >
           Tilføj spil
         </Button>
       </SimpleGrid>
@@ -68,13 +77,21 @@ export function StorePage() {
   };
 
   const featuredGames = games.slice(0, 5).map((item) => {
-    return <Carousel.Slide key={item.id}>
-      <GameCardLarge game={item} />
-    </Carousel.Slide>
+    return (
+      <Carousel.Slide key={item.id}>
+        <GameCardLarge game={item} />
+      </Carousel.Slide>
+    );
   });
 
   const gridElements = games.map((item) => (
-    <Paper key={item.id} shadow="md" withBorder radius="lg" p="lg">
+    <Paper
+      key={item.id}
+      shadow="md"
+      withBorder
+      radius="lg"
+      p="lg"
+    >
       <Flex direction={"column"} h={"100%"}>
         <Image
           style={{ aspectRatio: 16 / 9 }}
@@ -85,7 +102,7 @@ export function StorePage() {
         <Title order={3} pt="md">
           {item.title}
         </Title>
-        <Text color="gray" pb="md">
+        <Text c="gray" pb="md">
           {item.genres.join(", ")}
         </Text>
         <Text fz="sm" mt="xs">
@@ -107,20 +124,17 @@ export function StorePage() {
 
         <Box style={{ flexGrow: 1 }} />
 
-        <Group mt="xs">
-          {user && (
-            <>
-              {addGameButton(item)}
-            </>
-          )}
-        </Group>
+        <Group mt="xs">{user && <>{addGameButton(item)}</>}</Group>
       </Flex>
     </Paper>
   ));
 
-  if (loading) return <Center pt={"xl"}>
-    <Loader />
-  </Center>
+  if (loading)
+    return (
+      <Center pt={"xl"}>
+        <Loader />
+      </Center>
+    );
 
   return (
     <>
@@ -138,20 +152,20 @@ export function StorePage() {
           {featuredGames}
         </Carousel>
       </Box>
-      <Box bg={theme.colors[theme.primaryColor][0]} p={"md"}
+      <Box
+        bg={theme.colors[theme.primaryColor][0]}
+        p={"md"}
         style={{
           position: "sticky",
-          top: 0
-        }}>
+          top: 0,
+        }}
+      >
         <Title order={4}>Filtrer</Title>
-        <Flex
-          gap={"md"}
-          direction={"row"}
-        >
+        <Flex gap={"md"} direction={"row"}>
           <Input
             placeholder="Søg efter spil"
             style={{
-              flexGrow: 3
+              flexGrow: 3,
             }}
             onChange={(e) => {
               setSearch(e.currentTarget.value);
@@ -159,7 +173,7 @@ export function StorePage() {
           />
           <MultiSelect
             style={{
-              flexGrow: 1
+              flexGrow: 1,
             }}
             data={[
               "Action",
@@ -173,16 +187,14 @@ export function StorePage() {
               "Sports",
               "Strategy",
             ]}
-
             placeholder="Genres"
-
             onChange={(value) => {
               setGenres(value);
             }}
           />
           <TagsInput
             style={{
-              flexGrow: 1
+              flexGrow: 1,
             }}
             placeholder="Tags"
             value={tags}
